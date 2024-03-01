0fc2838f12a81cc56f078465932c5ebe2174f774
Ituki Kirihara <ituki@fc.to>
2024-03-01 23:02:58 +0900

    読み込み順変更

--- make.sh
+++ make.sh
@@ -20,6 +20,21 @@ fi
 mkdir -p "$TMP"
 mkdir -p "$OUT"
 
+FIRSTLOAD_FILES=("functions.js")
+SECONDLOAD_DIRS=("utils")
+
+function chk_loaded() {
+  local files=$1
+  local fname=$2
+  local i=""
+  for i in ${files[@]}; do
+    if [ "$i" == "$fname" ]; then
+      return 1
+    fi
+  done
+  return 0
+}
+
 function build () {
   local _dir=${1}
   local _lv=`expr 1 + ${2}`
@@ -37,17 +52,43 @@ function build () {
   s2dir=`echo ${sdir} | sed -E "s/^.*\.([^.]+)/\1/"`
   mkdir -p "${TMP}/${_dir}"
   touch "${TMP}/${_dir}/center.tjs"
-  for dir in `find "${SRC}/${MAIN}/${_dir}" -maxdepth 1 -type d -not -regex '.*/\..*' -printf '%P\n' | sed -E -e '/^[^a-zA-Z0-9]/s/^/#_/' -e '/^[0-9]/s/^/1_/' -e '/^[a-z]/s/^/2_/' -e '/^[A-Z]/s/^/3_/' | sort | sed -e 's/^._//'`; do
-    target=${_dir}/${dir}
-    build $target $_lv
-    cat "${TMP}/${target}/module.tjs" >> "${TMP}/${_dir}/center.tjs"
+
+  for file in ${FIRSTLOAD_FILES[@]}; do
+    if [ -f "${SRC}/${MAIN}/${_dir}/${file}" ]; then
+      echo "// ================================================" >> "${TMP}/${_dir}/center.tjs"
+      echo "// module: $sdir , from: $file" >> "${TMP}/${_dir}/center.tjs"
+      echo "// ================================================" >> "${TMP}/${_dir}/center.tjs"
+      cat "${SRC}/${MAIN}/${_dir}/${file}" >> "${TMP}/${_dir}/center.tjs"
+      echo "" >> "${TMP}/${_dir}/center.tjs"
+    fi
   done
+
+  for dir in ${SECONDLOAD_DIRS[@]}; do
+    if [ -d "${SRC}/${MAIN}/${_dir}/${dir}" ]; then
+      target=${_dir}/${dir}
+      build $target $_lv
+      cat "${TMP}/${target}/module.tjs" >> "${TMP}/${_dir}/center.tjs"
+    fi
+  done
+
+  for dir in `find "${SRC}/${MAIN}/${_dir}" -maxdepth 1 -type d -not -regex '.*/\..*' -printf '%P\n' | sed -E -e '/^[^a-zA-Z0-9]/s/^/#_/' -e '/^[0-9]/s/^/1_/' -e '/^[a-z]/s/^/2_/' -e '/^[A-Z]/s/^/3_/' | sort | sed -e 's/^._//'`; do
+    chk_loaded $SECONDLOAD_DIRS $dir
+    if [ $? -eq 0 ]; then
+      target=${_dir}/${dir}
+      build $target $_lv
+      cat "${TMP}/${target}/module.tjs" >> "${TMP}/${_dir}/center.tjs"
+    fi
+  done
+
   for file in `find "${SRC}/${MAIN}/${_dir}" -maxdepth 1 -type f -regex '.+\.js' -printf '%P\n' | sed -E -e '/^[^a-zA-Z0-9]/s/^/#_/' -e '/^[0-9]/s/^/1_/' -e '/^[a-z]/s/^/2_/' -e '/^[A-Z]/s/^/3_/' | sort | sed -e 's/^._//'`; do
-    echo "// ================================================" >> "${TMP}/${_dir}/center.tjs"
-    echo "// module: $sdir , from: $file" >> "${TMP}/${_dir}/center.tjs"
-    echo "// ================================================" >> "${TMP}/${_dir}/center.tjs"
-    cat "${SRC}/${MAIN}/${_dir}/${file}" >> "${TMP}/${_dir}/center.tjs"
-    echo "" >> "${TMP}/${_dir}/center.tjs"
+    chk_loaded $FIRSTLOAD_FILES $file
+    if [ $? -eq 0 ]; then
+      echo "// ================================================" >> "${TMP}/${_dir}/center.tjs"
+      echo "// module: $sdir , from: $file" >> "${TMP}/${_dir}/center.tjs"
+      echo "// ================================================" >> "${TMP}/${_dir}/center.tjs"
+      cat "${SRC}/${MAIN}/${_dir}/${file}" >> "${TMP}/${_dir}/center.tjs"
+      echo "" >> "${TMP}/${_dir}/center.tjs"
+    fi
   done
   if [ $_lv -eq 1 ]; then
     cat "${SRC}/${TEMPLATE}/${MODBASETOP}" "${TMP}/${_dir}/center.tjs" "${SRC}/${TEMPLATE}/${MODBASEBOTTOM}" > "${TMP}/${_dir}/module.tjs"
@@ -60,17 +101,40 @@ function build () {
 
 touch "${TMP}/center.tjs"
 
+for file in ${FIRSTLOAD_FILES[@]}; do
+  if [ -f "${SRC}/${MAIN}/${file}" ]; then
+    echo "// ================================================" >> "${TMP}/center.tjs"
+    echo "// source: $file" >> "${TMP}/center.tjs"
+    echo "// ================================================" >> "${TMP}/center.tjs"
+    cat "${SRC}/${MAIN}/${file}" >> "${TMP}/center.tjs"
+    echo "" >> "${TMP}/center.tjs"
+  fi
+done
+
+for dir in ${SECONDLOAD_DIRS[@]}; do
+  if [ -d "${SRC}/${MAIN}/${dir}" ]; then
+    build $dir 0
+    cat "${TMP}/${dir}/module.tjs" >> "${TMP}/center.tjs"
+  fi
+done
+
 for dir in `find "${SRC}/${MAIN}/" -maxdepth 1 -type d -not -regex '.*/\..*' -printf '%P\n' | sed -E -e '/^[^a-zA-Z0-9]/s/^/#_/' -e '/^[0-9]/s/^/1_/' -e '/^[a-z]/s/^/2_/' -e '/^[A-Z]/s/^/3_/' | sort | sed -e 's/^._//'`; do
-  build $dir 0
-  cat "${TMP}/${dir}/module.tjs" >> "${TMP}/center.tjs"
+  chk_loaded $SECONDLOAD_DIRS $dir
+  if [ $? -eq 0 ]; then
+    build $dir 0
+    cat "${TMP}/${dir}/module.tjs" >> "${TMP}/center.tjs"
+  fi
 done
 
 for file in `find "${SRC}/${MAIN}/" -maxdepth 1 -type f -regex '.+\.js' -printf '%P\n' | sed -E -e '/^[^a-zA-Z0-9]/s/^/#_/' -e '/^[0-9]/s/^/1_/' -e '/^[a-z]/s/^/2_/' -e '/^[A-Z]/s/^/3_/' | sort | sed -e 's/^._//'`; do
-  echo "// ================================================" >> "${TMP}/center.tjs"
-  echo "// source: $file" >> "${TMP}/center.tjs"
-  echo "// ================================================" >> "${TMP}/center.tjs"
-  cat "${SRC}/${MAIN}/${file}" >> "${TMP}/center.tjs"
-  echo "" >> "${TMP}/center.tjs"
+  chk_loaded $FIRSTLOAD_FILES $file
+  if [ $? -eq 0 ]; then
+    echo "// ================================================" >> "${TMP}/center.tjs"
+    echo "// source: $file" >> "${TMP}/center.tjs"
+    echo "// ================================================" >> "${TMP}/center.tjs"
+    cat "${SRC}/${MAIN}/${file}" >> "${TMP}/center.tjs"
+    echo "" >> "${TMP}/center.tjs"
+  fi
 done
 
 cat "${SRC}/${TEMPLATE}/${JSTOP}" "${TMP}/center.tjs" "${SRC}/${TEMPLATE}/${JSBOTTOM}" > "${OUT}/${OUTFILE}"
